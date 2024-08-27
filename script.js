24e64db5104747cdbaa59ca57b20eeb04a03ee6f
Oliv-19 <betza.bruguera@gmail.com>
2024-08-27 13:38:49 -0400

    Display book content in cards

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let dialog = {
   form: document.getElementById('form'),
   closeButton: document.getElementById('close'),
   showButton: document.getElementById('addBook'),
+  id: 0,
   addListeners: function(){
     this.showButton.addEventListener("click", ()=> this.modal.showModal());
     this.closeButton.addEventListener("click", ()=> this.modal.close());
@@ -13,35 +14,88 @@ let dialog = {
           let formData = new FormData(form)
           console.log(formData.get('read'))
           this.modal.close()
-
-          addBookToLibrary(formData)
-          displayBook()
+          
+          this.id++
+          createBook(formData, this.id)
+          
       })
     
   }
   
 }
 
+
+
 dialog.addListeners()
 
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(form) {
+function Book(form, id) {
     this.title = form.get('title')
     this.author = form.get('author')
     this.pages = form.get('pages')
     this.read = form.get('read')
+    this.book_id = id
+
 }
 
-function addBookToLibrary(form) {
-    let newBook = new Book(form)
+function createBook(form, id){
+  let newBook = new Book(form, id)
+  addBookToLibrary(newBook)
+}
+
+
+function addBookToLibrary(newBook) {
     myLibrary.push(newBook)
+    displayBook(newBook)
     // console.log(myLibrary)
 }
 
-function displayBook(){
-    for (let i = 0; i < myLibrary.length; i++){
-        console.log(myLibrary[i])
+let bookCard = {
+  bookContainer: document.querySelector('.bookDisplay'),
 
-    }
+  createHtmlElems:function (tagName, value, classId){
+    elem= document.createElement(tagName)
+    elem.classname = classId
+    elem.textContent= value
+    return elem
+  },
+
+  htmlElems: function (book){
+      let b_title = this.createHtmlElems('h3',book.title, 'b_title')
+      let b_author = this.createHtmlElems('h3',book.author, 'b_author')
+      let b_pages = this.createHtmlElems('h3',book.pages, 'b_pages')
+      let b_read = this.createHtmlElems('h3',book.read, 'b_read')
+      
+      let htmlElems= [b_title, b_author, b_pages, b_read]
+      
+      return htmlElems
+    },
+
+  createCardElem: function(book){
+    let card = document.createElement('div');
+    card.className= 'book'
+    card.dataset.indexNumber = book.book_id ;
+
+    this.htmlElems(book).forEach(elem => {
+      console.log(elem)
+      card.appendChild(elem)
+    });
+    //console.log(this.htmlElems(book))
+    this.bookContainer.appendChild(card)
+    
+
+  }
+}
+
+function displayBook(book){
+  console.log(book)
+  bookCard.createCardElem(book)
+ 
+  
+
+    // for (let i = 0; i < myLibrary.length; i++){
+    //     console.log(myLibrary)
+    //     bookCard.createCardElem(myLibrary)
+    // }
 }
